4f720ac35491f8ebe8a2a573baa06d4f7601acb8
liuleijie <lei2264208365@outlook.com>
2025-08-28 15:03:44 +0800

    添加配置，将txt文件识别为lua文件

--- init.lua
+++ init.lua
@@ -2,3 +2,12 @@ require("lazyInit")
 require("colorscheme")
 require("core.options")
 require("core.keymaps")
+
+-- 在 init.lua 中添加
+vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
+    pattern = "*.txt",
+    callback = function()
+        vim.bo.filetype = "lua"  -- 设置文件类型为 lua
+    end
+})
+
