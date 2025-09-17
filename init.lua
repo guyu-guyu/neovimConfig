144c43e917da05a4a47321ecb72789cbc5b42ba0
liuleijie <lei2264208365@outlook.com>
2025-09-17 10:59:49 +0800

    添加markdown文件checkBox交互插件及配置快捷键

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ require("lazyInit")
 require("colorscheme")
 require("core.options")
 require("core.keymaps")
+require("markview_extras_checkboxes")
 
 -- 在 init.lua 中添加
 vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
