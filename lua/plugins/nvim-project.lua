d20b8ac7b087cf39784d0fa83fe78ef2eef693d4
谷雨 <lei2264208365@outlook.com>
2025-09-22 20:05:37 +0800

    - 提交

---
+++ lua/plugins/nvim-project.lua
@@ -0,0 +1,12 @@
+-- Lua
+return{
+  "ahmedkhalf/project.nvim",
+  config = function()
+    require("project_nvim").setup {
+      patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json" },
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+}
